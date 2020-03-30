30ccd4325819d6946fc25f934e9dcc400611415c
fbarrientosj <fnbarrientos@uc.cl>
2020-03-30 18:32:55 -0300

    first push

---
+++ flaskapi.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for
+import requests
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+
+    url1 = "https://rickandmortyapi.com/api/episode/"
+    url2 = "https://rickandmortyapi.com/api/episode?page=2"
+    payload = {}
+    headers= {}
+    response1 = requests.request("GET", url1, headers=headers, data = payload)
+    response2 = requests.request("GET", url2, headers=headers, data = payload)
+    data1 = response1.json() 
+    data2 = response2.json() 
+    episodes1 = data1['results']
+    episodes2 = data2['results']
+    episodes = episodes1 + episodes2
+    return render_template('home.html', episodes=episodes)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,4 @@
+lista = [1,2,3]
+adding = [4, 5, 6]
+
+print(lista + adding)
